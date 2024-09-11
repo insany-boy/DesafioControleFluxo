ad1c9fbedbfb9d9ceb2b6ae8f53bf64d1de7f84d
insany-boy <kauanmeirelles1008@gmail.com>
2024-09-11 10:36:16 -0300

    Atualizando o desafio

---
+++ src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException e) {
+            //Imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+
+            System.out.println(e.getMessage());
+        }
+
+        static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+            //validar se parametroUm é MAIOR que parametroDois e lan~çar a exceção
+
+            if(parametroUm >= patametroDois) {
+                throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+            }
+        }
+    }
+}
