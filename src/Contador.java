93d1c3a4e75e165b13d0c80efc6daaad331a4c02
insany-boy <kauanmeirelles1008@gmail.com>
2024-09-11 10:59:10 -0300

    Concluindo o código

--- src/Contador.java
+++ src/Contador.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Contador {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Scanner terminal = new Scanner(System.in);
 
@@ -13,17 +17,20 @@ public class Contador {
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 
-        } catch (ParametrosInvalidosException e) {
+        } catch (ParametrosInvalidosException exception) {
             //Imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
 
-            System.out.println(e.getMessage());
+            System.out.println(exception.getMessage());
+        } finally {
+            terminal.close();
         }
+      }
 
         static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
 
             //validar se parametroUm é MAIOR que parametroDois e lan~çar a exceção
 
-            if(parametroUm >= patametroDois) {
+            if(parametroUm >= parametroDois) {
                 throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
             }
 
@@ -35,5 +42,5 @@ public class Contador {
                 System.out.println("Imprimindo o número " + i);
             }
         }
-    }
+    
 }
